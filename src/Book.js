10ca0d58b6f42955a490b282197c9553424e0175
krzysztofHadas <hada10@gazeta.pl>
2018-06-15 09:22:21 +0200

    Book refactored as a functional component

--- src/Book.js
+++ src/Book.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react'
 import Changer from './Changer'
 
-class Book extends Component {
+const Book = (props) => {
 
-    render() {
-        const { title, imageLinks, authors, shelf } = this.props.book;
-        let bookCoverImage = '';
-        if (imageLinks && imageLinks.thumbnail) {
-            bookCoverImage = `url("${imageLinks.thumbnail}")`;
-        }
-        const bookCoverStyle = {
-            width: 128,
-            height: 193,
-            backgroundImage: bookCoverImage
-        }
-        return (
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={bookCoverStyle}></div>
-                        <Changer shelf={shelf} shelfSelected={(e) => {
-                            this.props.changerSwitched(e.target.value, this.props.book);
-                        }} />
-                    </div>
-                    <div className="book-title">{title}</div>
-                    <div className="book-authors">{authors && authors.map((author) => {
-                        return (
-                            <div><span>{author}</span><br /></div>
-                        );
-                    })}</div>
-                </div>
-            </li>
-        );
+    const { title, imageLinks, authors, shelf } = props.book;
+    let bookCoverImage = '';
+    if (imageLinks && imageLinks.thumbnail) {
+        bookCoverImage = `url("${imageLinks.thumbnail}")`;
     }
+    const bookCoverStyle = {
+        width: 128,
+        height: 193,
+        backgroundImage: bookCoverImage
+    }
+    return (
+        <li>
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={bookCoverStyle}></div>
+                    <Changer shelf={shelf} shelfSelected={(e) => {
+                        props.changerSwitched(e.target.value, props.book);
+                    }} />
+                </div>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{authors && authors.map((author) => {
+                    return (
+                        <div><span>{author}</span><br /></div>
+                    );
+                })}</div>
+            </div>
+        </li>
+    );
+
 }
 
 export default Book;
